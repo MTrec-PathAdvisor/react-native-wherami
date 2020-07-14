d8b6316e74cdc17ee1f12a13e78e985a2d028356
MTrec <mtrec@MacBook-Pro.lan>
2020-07-14 17:33:03 +0800

    ios bridge prototype

--- ios/Wherami.h
+++ ios/Wherami.h
@@ -1,5 +1,7 @@
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#import <LBSOfflineSDK/LBSOfflineSDK.h>
 
-@interface Wherami : NSObject <RCTBridgeModule>
+@interface Wherami : RCTEventEmitter <RCTBridgeModule,LocationEngineDelegate>
 
 @end
--- ios/Wherami.m
+++ ios/Wherami.m
@@ -3,12 +3,78 @@
 
 @implementation Wherami
 
+bool needPermission=TRUE;
+
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
-{
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+- (NSArray<NSString *> *)supportedEvents{
+    return@[@"onReady",@"onEngineStarted",@"onEngineStopped",@"onLocationUpdate",@"requestPermission",@"message"];
 }
+
+RCT_EXPORT_METHOD(checkSelfPermission)
+{
+  NSLog(@"ios:Check Self Permission");
+
+  if(needPermission){
+      NSDictionary* params= @{@"missingPermissions": @"missingPermissions"};
+                   
+    [self sendEventWithName:@"requestPermission" body:params];
+  }else{
+      //init sdk
+  }
+};
+    
+RCT_EXPORT_METHOD(initializeSDK)
+{
+    NSLog(@"ios:initialize ios-SDK");
+    [LocationEngine engine];
+    [[LocationEngine engine] setDelegate:self];
+    [[LocationEngine engine] authorityNotify:self];
+    // sendEvent(reactContext, "onReady", null);
+    [self sendEventWithName:@"onReady" body:nil];
+    
+};
+                  
+RCT_EXPORT_METHOD(start)
+{
+    NSLog(@"ios:start engine");
+    [[LocationEngine engine] turnOnPosition];
+    [self sendEventWithName:@"onEngineStarted" body:nil];
+    // sendEvent(reactContext, "onEngineStarted", null);
+    
+};
+                  
+RCT_EXPORT_METHOD(stop)
+{
+    NSLog(@"ios:stop engine");
+    [[LocationEngine engine] turnOffPosition];
+    [self sendEventWithName:@"onEngineStopped" body:nil];
+    // sendEvent(reactContext, "onEngineStopped", null);
+};
+    
+// - (void) sendEvent:(ReactContext) reactContext :(String *) eventName : @Nullable (WritableMap) params {
+//     reactContext.
+//             getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+//             .emit(eventName, params);
+// }
+
+#pragma mark - LocationEngineDelegate
+
+- (void)didUpdateLocation:(HistoryCaled *)history{
+    if (!history) {
+        [self sendEventWithName:@"onLocationUpdate" body:@{@"location":nil}];
+        return;
+    }else{
+        NSDictionary * locationMap =@{
+                             @"x" : history.location.x,
+                             @"y" : history.location.y,
+                             @"areaId" : history.level,
+                             @"radius": @"location.radius"
+                             };
+        [self sendEventWithName:@"onLocationUpdate" body:@{@"location":locationMap}];
+        
+    }
+    
+};
 
 @end
